5621ff0b5dff12b2b42303782d197f93791a684b
anna <annavyly@gmail.com>
2025-01-06 08:50:40 +0000

    0. New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: pointer to the parent node of node to create.
+ * @value: value to assign to new node
+ *
+ * Return: pointer to new node, otherwise NULL.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newTreeNode;
+
+	newTreeNode->n = value;
+	newTreeNode->left = NULL;
+	newTreeNode->right = NULL;
+
+	return(newTreeNode);
+}
