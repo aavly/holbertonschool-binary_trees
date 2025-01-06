f752478b5ed3afe8a5aaa64fef17f9455f1682ff
anna <annavyly@gmail.com>
2025-01-06 08:59:32 +0000

    correcting sandbox errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newTreeNode;
 
+	newTreeNode = malloc(sizeof(binary_tree_t));
+	if (newTreeNode == NULL)
+		return (NULL);
+
 	newTreeNode->n = value;
 	newTreeNode->left = NULL;
 	newTreeNode->right = NULL;
+
 
 	return(newTreeNode);
 }
