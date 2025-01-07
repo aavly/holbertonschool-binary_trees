2182737b04574eaf0ccc01d79a2d3738318debc8
anna <annavyly@gmail.com>
2025-01-07 01:26:38 +0000

    equal to or greater than

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -21,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	leftHeight = binary_tree_height(tree->left);
 	rightHeight = binary_tree_height(tree->right);
 
-	if (leftHeight > rightHeight)
+	if (leftHeight >= rightHeight)
 		return (leftHeight);
 	else
 		return (rightHeight);
