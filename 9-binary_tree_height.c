d5e4dafb5744b52d732fe26efafc0c02aea0980f
anna <annavyly@gmail.com>
2025-01-07 01:29:59 +0000

    removed initialisation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,16 +15,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	leftHeight = 0;
-	rightHeight = 0;
-
 	leftHeight = binary_tree_height(tree->left);
 	rightHeight = binary_tree_height(tree->right);
 
 	if (leftHeight >= rightHeight)
-		return (leftHeight);
+		return (leftHeight + 1);
 	else
-		return (rightHeight);
+		return (rightHeight + 1);
 
 
 
