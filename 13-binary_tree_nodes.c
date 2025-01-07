5bc5ce454f79fe7ffdf88fae5ee5f55a05b30e1a
anna <annavyly@gmail.com>
2025-01-07 14:13:11 +0000

    fixing node count logic

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,9 +19,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	nodesCount = 0;
 
 	if (tree->left != NULL || tree->right != NULL)
-		return (1);
+		nodesCount = 1;
 
-	nodesCount = binary_tree_nodes(tree->left) +
+	nodesCount += binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right);
 
 	return (nodesCount);
