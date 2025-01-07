d40771fad8caa069ab12f4e53431c1bef11b4c3f
anna <annavyly@gmail.com>
2025-01-07 01:22:48 +0000

    removed +1 for measuring height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,9 +22,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	rightHeight = binary_tree_height(tree->right);
 
 	if (leftHeight > rightHeight)
-		return (leftHeight + 1);
+		return (leftHeight);
 	else
-		return (rightHeight + 1);
+		return (rightHeight);
 
 
 
